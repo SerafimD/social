00a14f83f640630a190a5bf2f6ce235d0d97789b
KateGL <yolkate@gmail.com>
2014-12-27 10:10:42 +0200

    Добавление модели Post с тестом

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,5 @@ class User < ActiveRecord::Base
   has_many :community, through: :community_memberships, dependent: :destroy
   has_many :user_owner_relations, :class_name =>'UserRelation', :foreign_key =>"owner_id", :inverse_of => :user_owner, dependent: :destroy
   has_many :user_rel_relations, :class_name => 'UserRelation', :foreign_key =>"user_rel_id", :inverse_of => :user_rel, dependent: :destroy
+  has_many :posts, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141227031330) do
+ActiveRecord::Schema.define(version: 20141227075558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,14 @@ ActiveRecord::Schema.define(version: 20141227031330) do
   end
 
   add_index "people", ["user_id"], name: "index_people_on_user_id", using: :btree
+
+  create_table "posts", force: true do |t|
+    t.text     "content"
+    t.datetime "publish_date"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "user_relations", force: true do |t|
     t.integer  "user_owner_id"
