068e7eadc20cbc2e25f9d2521a72458bede731c2
KateGL <yolkate@gmail.com>
2014-12-27 19:07:42 +0200

    Убрала закомментированный код в фабрике

--- spec/factories/people.rb
+++ spec/factories/people.rb
@@ -2,8 +2,6 @@ FactoryGirl.define do
   factory :person do
     surename {Faker::Name.last_name}
     date_of_birth {Faker::Time.between(10000.days.ago,10000.days.ago)}
-    #user_id { Factory(:user)}
-    #user
     association :user
   end
 
